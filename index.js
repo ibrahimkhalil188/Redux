a1bc397d69116f3461093c59b864633eb473a580
unknown <ibrahimkhalil.mine@gmail.com>
2022-06-23 16:46:26 +0600

    counter app

--- index.js
+++ index.js
@@ -1,47 +1,77 @@
-//state
-//action
-//reducer
-//store
 
-import { StoreCreator } from "redux"
+// import for create store
+
+const { createStore } = require("redux")
+
+//Defining constants
 
 const INCREMENT = "INCREMENT"
 const DECREMENT = "DECREMENT"
 const RESET = "RESET"
 
+//initial state of counter
+
 const counterState = {
     count: 0
 }
 
+// action and action type 
+
 const increment = () => {
-    type: INCREMENT
+    return {
+        type: INCREMENT
+    }
 }
 const decrement = () => {
-    type: DECREMENT
+    return {
+        type: DECREMENT
+    }
 }
 const reset = () => {
-    type: RESET
+    return {
+        type: RESET
+    }
 }
+
+// reducer
 
 const counterReducer = (state = counterState, action) => {
     switch (action.type) {
         case INCREMENT: {
-            count: state.count + 1
+            return {
+                count: state.count + 1
+            }
         }
         case DECREMENT: {
-            count: state.count - 1
+            return {
+                count: state.count - 1
+            }
         }
-        case DECREMENT: {
-            count: 0
+        case RESET: {
+            return {
+                count: 0
+            }
         }
         default: return state
     }
 }
 
-const store = configureStore(counterReducer)
+// create a store
+
+const store = createStore(counterReducer)
+
+// subscribe the store
 
 store.subscribe(() => {
     console.log(store.getState())
 })
 
+//dispatch the store or click event or button in real life 
+
 store.dispatch(increment())
+store.dispatch(increment())
+store.dispatch(increment())
+store.dispatch(decrement())
+store.dispatch(increment())
+store.dispatch(reset())
+
