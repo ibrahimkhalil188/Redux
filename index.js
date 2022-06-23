ed054f0c52952463add201c869394f87cc93a203
unknown <ibrahimkhalil.mine@gmail.com>
2022-06-23 17:01:44 +0600

    payload

--- index.js
+++ index.js
@@ -8,12 +8,13 @@ const { createStore } = require("redux")
 const INCREMENT = "INCREMENT"
 const DECREMENT = "DECREMENT"
 const RESET = "RESET"
-
+const INCREMENT_BY_VALUE = "INCREMENT_BY_VALUE"
 //initial state of counter
 
 const counterState = {
     count: 0
 }
+
 
 // action and action type 
 
@@ -33,22 +34,39 @@ const reset = () => {
     }
 }
 
+// use case of payload. it update the value by providing value.
+const incrementByValue = (value) => {
+    return {
+        type: INCREMENT_BY_VALUE,
+        payload: value
+    }
+}
+
 // reducer
 
 const counterReducer = (state = counterState, action) => {
     switch (action.type) {
         case INCREMENT: {
             return {
+                ...state,
                 count: state.count + 1
             }
         }
         case DECREMENT: {
             return {
+                ...state,
                 count: state.count - 1
+            }
+        }
+        case INCREMENT_BY_VALUE: {
+            return {
+                ...state,
+                count: state.count + action.payload
             }
         }
         case RESET: {
             return {
+                ...state,
                 count: 0
             }
         }
@@ -74,4 +92,5 @@ store.dispatch(increment())
 store.dispatch(decrement())
 store.dispatch(increment())
 store.dispatch(reset())
+store.dispatch(incrementByValue(5))
 
